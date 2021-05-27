51fc74f3afae3e9a606e4646589a5bb4a7832974
Lucas Viana <oviana@outlook.com>
2021-05-27 20:53:57 -0300

    hopefully the last commit - zipped source code

--- code/main.c
+++ code/main.c
@@ -10,17 +10,28 @@
 #include "headers/util.h"
 
 int main(){
+    // function --> functionality to execute (1, 2, 3, 4, 5, 6, 7, 8)
+    // n --> number of inputs to be read in functions 7 and 8
     int function, n;
+
+    // auxiliar variables that'll be used to read input from the stdin
     char auxString1[50], auxString2[50], auxString3[50];
+
+    // variable that'll be used to read the .csv files
     char *content;
     
     scanf("%d %s", &function, auxString1);
 
     switch(function){
+        // functionality 1
         case 1:
+            // reads binary file name
             scanf("%s", auxString2);
             
+            // reads .csv file
             content = read_csv(auxString1);
+
+            // parses .csv content and writes to binary
             write_vehicle_bin(auxString2, content);
             
             binarioNaTela(auxString2);
@@ -28,10 +39,15 @@ int main(){
 
             break;
             
+        // functionality 2
         case 2:
+            // reads binary file name
             scanf("%s", auxString2);
 
+            // reads .csv file
             content = read_csv(auxString1);
+
+            // parses .csv content and writes to binary
             write_line_bin(auxString2, content);
 
             binarioNaTela(auxString2);
@@ -39,49 +55,62 @@ int main(){
 
             break;
 
+        // functionality 3
         case 3:
             print_vehicle_bin(auxString1);
             break;
 
+        // functionality 4
         case 4:
             print_line_bin(auxString1);
             break;
 
+        // functionality 5
         case 5:
+            // reads query key-value pair
             scanf("%s", auxString2);
             scan_quote_string(auxString3);
             
+            // searches for the query in the database and prints results
             search_vehicle_bin(auxString1, auxString2, auxString3);
 
             break;
 
+        // functionality 6
         case 6:
+            // reads query key-value pair
             scanf("%s", auxString2);
             scan_quote_string(auxString3);
 
+            // searches for the query in the database and prints results
             search_line_bin(auxString1, auxString2, auxString3);
 
             break;
 
+        // functionality 7
         case 7:
+            // reads number of inputs to be read from stdin
             scanf("%d", &n);
 
+            // reads all inputs and appends their data to the specified binary file
             append_vehicle_bin(auxString1, n);
             binarioNaTela(auxString1);
 
             break;
 
+        // functionality 8
         case 8:
+            // reads number of inputs to be read from stdin
             scanf("%d", &n);
 
+            // reads all inputs and appends their data to the specified binary file
             append_line_bin(auxString1, n);
             binarioNaTela(auxString1);
 
             break;
 
-        default:
-            printf("Falha no processamento do arquivo.\n");
-            break;
+        // if an invalid funcionality was passed
+        default: raise_error("");
     }
     
     return 0;
