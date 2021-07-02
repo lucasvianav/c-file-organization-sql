20183c709b9158ca74449820b815f9ce99e32064
Lucas Viana <oviana@outlook.com>
2021-07-02 19:55:45 -0300

    reorganized btree recursive_insert()

--- code/source/btree.c
+++ code/source/btree.c
@@ -241,7 +241,9 @@ promotion_info split(promotion_info inserted, btree_page current_node, FILE *fil
 
 // internal function that'll recursively insert a key to the tree
 promotion_info recursive_insert(int current_rrn, int inserted_key, long long inserted_ref, FILE *file) {
-    promotion_info return_value;
+    // base case - if the current node does not exist "promotes"
+    // the inserted key (the past node was a leaf)
+    if (current_rrn == INVALID) { return (promotion_info) { inserted_key, inserted_ref, INVALID }; }
 
     // disk page struct
     btree_page node;
@@ -267,171 +269,161 @@ promotion_info recursive_insert(int current_rrn, int inserted_key, long long ins
     fread(&node.Pr4                , sizeof(long long) , 1 , file);
     fread(&node.P5                 , sizeof(int)       , 1 , file);
 
+    // if the inserted key already exists on the tree, raises error
+    if (
+        node.C1 == inserted_key
+        || node.C2 == inserted_key
+        || node.C3 == inserted_key
+        || node.C4 == inserted_key
+    ) { raise_error(""); }
 
-    // base case - if the current node does not exist "promotes"
-    // the inserted key (the past node was a leaf)
-    if (current_rrn == INVALID) {
-        return_value.key = inserted_key;
-        return_value.reference = inserted_ref;
-        return_value.child_node_rrn = INVALID;
+    // finds the position in which the child node
+    // in which the inserted key would enter
+    int child_rrn = INVALID;
+    if (inserted_key < node.C1) { child_rrn = node.P1; }
+    else if (inserted_key < node.C2) { child_rrn = node.P2; }
+    else if (inserted_key < node.C3) { child_rrn = node.P3; }
+    else if (inserted_key < node.C4) { child_rrn = node.P4; }
+    else { child_rrn = node.P5; }
+
+    promotion_info promotion = recursive_insert(child_rrn, inserted_key, inserted_ref, file);
+
+    if(promotion.key == INVALID){ return promotion; }
+
+    // if there is no space on the current node (split needed)
+    else if(node.nroChavesIndexadas < 4) { return split(promotion, node, file); }
+
+    // if there is not:
+
+    // inserts the promoted key to the current node while maintaining it sorted
+
+    // if the promoted key will enter at position 1
+    if(promotion.key < node.C1 && node.C1 != INVALID){
+        // bumps all other keys one position forward
+        node.C4  = node.C3;
+        node.Pr4 = node.Pr3;
+        node.P5  = node.P4;
+
+        node.C3  = node.C2;
+        node.Pr3 = node.Pr2;
+        node.P4  = node.P3;
+
+        node.C2  = node.C1;
+        node.Pr2 = node.Pr1;
+        node.P3  = node.P2;
+
+        // and sets it to position 1 (maintaining the preceding child node)
+        node.C1  = promotion.key;
+        node.Pr1 = promotion.reference;
+        node.P2  = promotion.child_node_rrn;
     }
 
-    // if the current node exists (the past node was not a leaf)
+    // if the promoted key will enter at position 2
+    else if(promotion.key < node.C2 && node.C2 != INVALID){
+        // bumps the next keys one position forward
+        node.C4  = node.C3;
+        node.Pr4 = node.Pr3;
+        node.P5  = node.P4;
+
+        node.C3  = node.C2;
+        node.Pr3 = node.Pr2;
+        node.P4  = node.P3;
+
+        // and sets it to position 2 (maintaining the preceding child node)
+        node.C2  = promotion.key;
+        node.Pr2 = promotion.reference;
+        node.P3  = promotion.child_node_rrn;
+    }
+
+    // if the promoted key will enter at position 3
+    else if(promotion.key < node.C3 && node.C3 != INVALID){
+        // bumps the next key one position forward
+        node.C4  = node.C3;
+        node.Pr4 = node.Pr3;
+        node.P5  = node.P4;
+
+        // and sets it to position 3 (maintaining the preceding child node)
+        node.C3  = promotion.key;
+        node.Pr3 = promotion.reference;
+        node.P4  = promotion.child_node_rrn;
+    }
+
+    // if the promoted key will enter at position 4
     else {
-        // if the inserted key already exists on the tree, raises error
-        if (node.C1 == inserted_key || node.C2 == inserted_key || node.C3 == inserted_key || node.C4 == inserted_key) { raise_error(""); }
-
-        // finds the position in which the child node
-        // in which the inserted key would enter
-        int child_rrn = INVALID;
-        if (inserted_key < node.C1) { child_rrn = node.P1; }
-        else if (inserted_key < node.C2) { child_rrn = node.P2; }
-        else if (inserted_key < node.C3) { child_rrn = node.P3; }
-        else if (inserted_key < node.C4) { child_rrn = node.P4; }
-        else { child_rrn = node.P5; }
-
-        promotion_info promotion = recursive_insert(child_rrn, inserted_key, inserted_ref, file);
-
-        if(promotion.key == INVALID){ return promotion; }
-
-        // if there is space on the current node (no split needed)
-        else if(node.nroChavesIndexadas < 4){
-            // inserts the promoted key to the current node while maintaining it sorted
-
-            // if the promoted key will enter at position 1
-            if(promotion.key < node.C1 && node.C1 != INVALID){
-                // bumps all other keys one position forward
-                node.C4  = node.C3;
-                node.Pr4 = node.Pr3;
-                node.P5  = node.P4;
-
-                node.C3  = node.C2;
-                node.Pr3 = node.Pr2;
-                node.P4  = node.P3;
-
-                node.C2  = node.C1;
-                node.Pr2 = node.Pr1;
-                node.P3  = node.P2;
-
-                // and sets it to position 1 (maintaining the preceding child node)
-                node.C1  = promotion.key;
-                node.Pr1 = promotion.reference;
-                node.P2  = promotion.child_node_rrn;
-            }
-
-            // if the promoted key will enter at position 2
-            else if(promotion.key < node.C2 && node.C2 != INVALID){
-                // bumps the next keys one position forward
-                node.C4  = node.C3;
-                node.Pr4 = node.Pr3;
-                node.P5  = node.P4;
-
-                node.C3  = node.C2;
-                node.Pr3 = node.Pr2;
-                node.P4  = node.P3;
-
-                // and sets it to position 2 (maintaining the preceding child node)
-                node.C2  = promotion.key;
-                node.Pr2 = promotion.reference;
-                node.P3  = promotion.child_node_rrn;
-            }
-
-            // if the promoted key will enter at position 3
-            else if(promotion.key < node.C3 && node.C3 != INVALID){
-                // bumps the next key one position forward
-                node.C4  = node.C3;
-                node.Pr4 = node.Pr3;
-                node.P5  = node.P4;
-
-                // and sets it to position 3 (maintaining the preceding child node)
-                node.C3  = promotion.key;
-                node.Pr3 = promotion.reference;
-                node.P4  = promotion.child_node_rrn;
-            }
-
-            // if the promoted key will enter at position 4
-            else {
-                // sets it to position 4 (maintaining the preceding child node)
-                node.C3  = promotion.key;
-                node.Pr3 = promotion.reference;
-                node.P4  = promotion.child_node_rrn;
-            }
-
-            // goes to the current register's "nroChavesIndexadas" field
-            fseek(file, current_node_position + sizeof(char), SEEK_SET);
-
-            // rewrites the current node
-            fwrite(&node.nroChavesIndexadas, sizeof(int),       1, file);
-            fwrite(&node.RRNdoNo,            sizeof(int),       1, file);
-            fwrite(&node.P1,                 sizeof(int),       1, file);
-            fwrite(&node.C1,                 sizeof(int),       1, file);
-            fwrite(&node.Pr1,                sizeof(long long), 1, file);
-            fwrite(&node.P2,                 sizeof(int),       1, file);
-            fwrite(&node.C2,                 sizeof(int),       1, file);
-            fwrite(&node.Pr2,                sizeof(long long), 1, file);
-            fwrite(&node.P3,                 sizeof(int),       1, file);
-            fwrite(&node.C3,                 sizeof(int),       1, file);
-            fwrite(&node.Pr3,                sizeof(long long), 1, file);
-            fwrite(&node.P4,                 sizeof(int),       1, file);
-            fwrite(&node.C4,                 sizeof(int),       1, file);
-            fwrite(&node.Pr4,                sizeof(long long), 1, file);
-            fwrite(&node.P5,                 sizeof(int),       1, file);
-
-            // no promotion happened
-            return_value.key = INVALID;
-            return_value.reference = INVALID;
-            return_value.child_node_rrn = INVALID;
-        }
-
-        else { return_value = split(promotion, node, file); }
+        // sets it to position 4 (maintaining the preceding child node)
+        node.C3  = promotion.key;
+        node.Pr3 = promotion.reference;
+        node.P4  = promotion.child_node_rrn;
     }
 
-    return return_value;
+    // goes to the current register's "nroChavesIndexadas" field
+    fseek(file, current_node_position + sizeof(char), SEEK_SET);
+
+    // rewrites the current node
+    fwrite(&node.nroChavesIndexadas, sizeof(int),       1, file);
+    fwrite(&node.RRNdoNo,            sizeof(int),       1, file);
+    fwrite(&node.P1,                 sizeof(int),       1, file);
+    fwrite(&node.C1,                 sizeof(int),       1, file);
+    fwrite(&node.Pr1,                sizeof(long long), 1, file);
+    fwrite(&node.P2,                 sizeof(int),       1, file);
+    fwrite(&node.C2,                 sizeof(int),       1, file);
+    fwrite(&node.Pr2,                sizeof(long long), 1, file);
+    fwrite(&node.P3,                 sizeof(int),       1, file);
+    fwrite(&node.C3,                 sizeof(int),       1, file);
+    fwrite(&node.Pr3,                sizeof(long long), 1, file);
+    fwrite(&node.P4,                 sizeof(int),       1, file);
+    fwrite(&node.C4,                 sizeof(int),       1, file);
+    fwrite(&node.Pr4,                sizeof(long long), 1, file);
+    fwrite(&node.P5,                 sizeof(int),       1, file);
+
+    // no promotion happened
+    return (promotion_info) { INVALID, INVALID, INVALID };
 }
 
-int __btree_search(int RRN, int inserted_key, FILE *ARQ){
-    btree_page page;
+long long recursive_search(int current_rrn, int queried_key, FILE *file) {
+    // base case - the current node does not exist
+    if (current_rrn == INVALID) { return INVALID; }
 
-    if (RRN == -1){
-        return -1;
-    }
-    else{
-        fseek(ARQ, (RRN+1)*DISK_PAGE_LENGTH, SEEK_SET);
+    // btree node struct
+    btree_page node;
 
-        fread(&page.folha, sizeof(char), 1, ARQ);
-        fread(&page.nroChavesIndexadas, sizeof(int), 1, ARQ);
-        fread(&page.RRNdoNo, sizeof(int), 1, ARQ);
-        fread(&page.P1, sizeof(int), 1, ARQ);
-        fread(&page.C1, sizeof(int), 1, ARQ);
-        fread(&page.Pr1, sizeof(long long), 1, ARQ);
-        fread(&page.P2, sizeof(int), 1, ARQ);
-        fread(&page.C2, sizeof(int), 1, ARQ);
-        fread(&page.Pr2, sizeof(long long), 1, ARQ);
-        fread(&page.P3, sizeof(int), 1, ARQ);
-        fread(&page.C3, sizeof(int), 1, ARQ);
-        fread(&page.Pr3, sizeof(long long), 1, ARQ);
-        fread(&page.P4, sizeof(int), 1, ARQ);
-        fread(&page.C4, sizeof(int), 1, ARQ);
-        fread(&page.Pr4, sizeof(long long), 1, ARQ);
-        fread(&page.P5, sizeof(int), 1, ARQ);
+    // goes to the current RRN's positio
+    fseek_rrn(file, current_rrn);
 
-        if (inserted_key < page.C1){
-            return (search(page.P1, inserted_key, ARQ));
-        }
-        else if ((page.C1 < inserted_key) && ( ((inserted_key < page.C2) && (page.nroChavesIndexadas > 1)) || ((page.nroChavesIndexadas == 1)))){
-            return (search(page.P2, inserted_key, ARQ));
-        }
-        else if ((page.C2 < inserted_key) && ( ((inserted_key < page.C3) && (page.nroChavesIndexadas > 2)) || ((page.nroChavesIndexadas == 2)))){
-            return (search(page.P3, inserted_key, ARQ));
-        }
-        else if ((page.C3 < inserted_key) && ( ((inserted_key < page.C4) && (page.nroChavesIndexadas > 3)) || ((page.nroChavesIndexadas == 3)))){
-            return (search(page.P4, inserted_key, ARQ));
-        }
-        else if(page.C4 < inserted_key && (page.nroChavesIndexadas == 4)){
-            return (search(page.P5, inserted_key, ARQ));
-        }
-        else{
-            return RRN;
-        }
+    fread(&node.folha, sizeof(char), 1, file);
+    fread(&node.nroChavesIndexadas, sizeof(int), 1, file);
+    fread(&node.RRNdoNo, sizeof(int), 1, file);
+    fread(&node.P1, sizeof(int), 1, file);
+    fread(&node.C1, sizeof(int), 1, file);
+    fread(&node.Pr1, sizeof(long long), 1, file);
+    fread(&node.P2, sizeof(int), 1, file);
+    fread(&node.C2, sizeof(int), 1, file);
+    fread(&node.Pr2, sizeof(long long), 1, file);
+    fread(&node.P3, sizeof(int), 1, file);
+    fread(&node.C3, sizeof(int), 1, file);
+    fread(&node.Pr3, sizeof(long long), 1, file);
+    fread(&node.P4, sizeof(int), 1, file);
+    fread(&node.C4, sizeof(int), 1, file);
+    fread(&node.Pr4, sizeof(long long), 1, file);
+    fread(&node.P5, sizeof(int), 1, file);
+
+    if (queried_key < node.C1) {
+      return (recursive_search(node.P1, queried_key, file));
+    } else if ((node.C1 < queried_key) &&
+               (((queried_key < node.C2) && (node.nroChavesIndexadas > 1)) ||
+                ((node.nroChavesIndexadas == 1)))) {
+      return (recursive_search(node.P2, queried_key, file));
+    } else if ((node.C2 < queried_key) &&
+               (((queried_key < node.C3) && (node.nroChavesIndexadas > 2)) ||
+                ((node.nroChavesIndexadas == 2)))) {
+      return (recursive_search(node.P3, queried_key, file));
+    } else if ((node.C3 < queried_key) &&
+               (((queried_key < node.C4) && (node.nroChavesIndexadas > 3)) ||
+                ((node.nroChavesIndexadas == 3)))) {
+      return (recursive_search(node.P4, queried_key, file));
+    } else if (node.C4 < queried_key && (node.nroChavesIndexadas == 4)) {
+      return (recursive_search(node.P5, queried_key, file));
+    } else {
+      return current_rrn;
     }
 }
