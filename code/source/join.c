f2df52f4407f3adfc08a117cae3209ed5a1d1bca
Lucas Viana <oviana@outlook.com>
2021-07-16 23:55:51 -0300

    creates single_loop_join()

--- code/source/join.c
+++ code/source/join.c
@@ -31,6 +31,9 @@ void nested_loop_join(char *vehiclesFilename, char *linesFilename){
         raise_error("Registro inexistente.");
     }
 
+    // if any matching pair was found
+    int found_any = 0;
+
     // reads and prints each matching register
     // outer loop (through vehicles)
     int i = 0;
@@ -114,6 +117,8 @@ void nested_loop_join(char *vehiclesFilename, char *linesFilename){
             j++;
         }
 
+        found_any = 1;
+
         // frees allocated vehicle strings
         free(v_data.modelo);
         free(v_data.categoria);
@@ -122,9 +127,126 @@ void nested_loop_join(char *vehiclesFilename, char *linesFilename){
         i++;
     }
 
+    // if no matching pair was found
+    if(!found_any){ raise_error("Registro inexistente."); }
+
     return;
 }
 
 void single_loop_join(char *vehiclesFilename, char *linesFilename, char *linesBtreeFilename){
+    FILE *f_vehicles = open_validate_binary(vehiclesFilename, "rb");
+    FILE *f_lines = open_validate_binary(linesFilename, "rb");
+    FILE *f_btree = open_validate_binary(linesBtreeFilename, "rb");
+
+    // vehicle structs
+    vehicle_header   v_header; // vehicle header
+    vehicle_register v_data;   // vehicle data register
+
+    // line structs
+    line_header   l_header; // line header
+    line_register l_data;   // line data register
+
+    // reads both files' header's data
+    v_header = read_vehicle_header(f_vehicles);
+    l_header = read_line_header(f_lines);
+
+    // if either file has no register, raises error
+    if(!v_header.nroRegistros || !l_header.nroRegistros){
+        raise_error("Registro inexistente.");
+    }
+
+    // if any matching pair was found
+    int found_any = 0;
+
+    // reads and prints each matching register
+    // outer loop (through vehicles)
+    int i = 0;
+    while(i < v_header.nroRegRemovidos){
+        // reads the current vehicle's "removido" and "tamanhoRegistro" fields
+        fread(&v_data.removido, sizeof(char), 1, f_vehicles);
+        fread(&v_data.tamanhoRegistro, sizeof(int), 1, f_vehicles);
+
+        // if the current vehicle was removed, it'll not be printed
+        if(v_data.removido == '0'){
+            fseek(f_vehicles, v_data.tamanhoRegistro, SEEK_CUR);
+            continue;
+        }
+
+        // reads the current vehicle's remaining fixed size fields
+        fread(v_data.prefixo, sizeof(char), 5, f_vehicles);
+        fread(v_data.data, sizeof(char), 10, f_vehicles);
+        fread(&v_data.quantidadeLugares, sizeof(int), 1, f_vehicles);
+        fread(&v_data.codLinha, sizeof(int), 1, f_vehicles);
+
+        // searches for the code on the btree
+        // and recovers the register's byte offset
+        // (or -1 if it doesn't exist)
+        long long byte_offset = __btree_search(v_data.codLinha, f_btree);
+
+        // if that line doesn't exist, goes to the next one
+        if(byte_offset == -1){
+            i++;
+            continue;
+        }
+
+        // reads the current vehicle's "modelo" field (variable size)
+        fread(&v_data.tamanhoModelo, sizeof(int), 1, f_vehicles);
+        v_data.modelo = (char *)malloc(sizeof(char) * v_data.tamanhoModelo);
+        fread(v_data.modelo, sizeof(char), v_data.tamanhoModelo, f_vehicles);
+
+        // reads the current vehicle's "categoria" field (variable size)
+        fread(&v_data.tamanhoCategoria, sizeof(int), 1, f_vehicles);
+        v_data.categoria = (char *)malloc(sizeof(char) * v_data.tamanhoCategoria);
+        fread(v_data.categoria, sizeof(char), v_data.tamanhoCategoria, f_vehicles);
+
+        // goes to the start of the register's code field
+        // (not necessary to check if it's removed cause
+        // removed registers do not appear on the btree)
+        fseek(f_lines, byte_offset + 5, SEEK_SET);
+
+        // reads the current line's fixed size fields
+        fread(&l_data.codLinha,     sizeof(int),  1, f_lines);
+        fread(&l_data.aceitaCartao, sizeof(char), 1, f_lines);
+
+        // reads the current line's "nomeLinha" field (variable size)
+        fread(&l_data.tamanhoNome, sizeof(int),  1,                      f_lines);
+        l_data.nomeLinha = (char *)malloc(sizeof(char) * l_data.tamanhoNome);
+        fread(l_data.nomeLinha,    sizeof(char), l_data.tamanhoNome, f_lines);
+
+        // reads the current line's "corLinha" field (variable size)
+        fread(&l_data.tamanhoCor, sizeof(int),  1,                     f_lines);
+        l_data.corLinha = (char *)malloc(sizeof(char) * l_data.tamanhoCor);
+        fread(l_data.corLinha,    sizeof(char), l_data.tamanhoCor, f_lines);
+
+        // reads the current register's "nomeLinha" field (variable size)
+        fread(&l_data.tamanhoNome, sizeof(int), 1, f_lines);
+        l_data.nomeLinha = (char *)malloc(sizeof(char) * l_data.tamanhoNome);
+        fread(l_data.nomeLinha, sizeof(char), l_data.tamanhoNome, f_lines);
+
+        // reads the current register's "corLinha" field (variable size)
+        fread(&l_data.tamanhoCor, sizeof(int), 1, f_lines);
+        l_data.corLinha = (char *)malloc(sizeof(char) * l_data.tamanhoCor);
+        fread(l_data.corLinha, sizeof(char), l_data.tamanhoCor, f_lines);
+
+        // prints the data
+        print_vehicle(v_header, v_data, 0);
+        print_line(l_header, l_data, 1);
+
+        found_any = 1;
+
+        // frees allocated strings
+        free(l_data.nomeLinha);
+        free(l_data.corLinha);
+        free(v_data.modelo);
+        free(v_data.categoria);
+
+        // increments i
+        i++;
+    }
+
+    // if no matching pair was found
+    if(!found_any){ raise_error("Registro inexistente."); }
+
+    return;
 }
 
