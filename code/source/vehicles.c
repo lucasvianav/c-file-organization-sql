a23f90f0973b795cb422675e6df0905946f72351
Lucas Viana <oviana@outlook.com>
2021-07-18 12:15:10 -0300

    removes unecessary comments from vehicles.c

--- code/source/vehicles.c
+++ code/source/vehicles.c
@@ -246,7 +246,6 @@ FILE *fcreate_binary(char *filename){
 
 // PUBLIC FUNCTIONS
 
-// receives a filename, parses the content and writes it to the file
 void write_vehicle_bin(char *filename, char *content){
     // creates the binary file
     FILE *binary = fcreate_binary(filename);
@@ -274,7 +273,6 @@ void write_vehicle_bin(char *filename, char *content){
     return;
 }
 
-// receives a filename, reads "no_inputs" vehicles and appends it to the file
 void append_vehicle_bin(char *filename, int no_inputs){
     // string that has the .bin filepath (inside the "binaries" directory)
     char *filepath = get_filepath(filename, 'b');
@@ -351,8 +349,6 @@ void append_vehicle_bin(char *filename, int no_inputs){
     return;
 }
 
-// receives a filename, reads "no_inputs" vehicles and appends it to the file
-// also inserts it's reference on the passed btree
 void append_vehicle_bin_btree(char *vehiclesFilename, char *btreeFilename, int no_inputs){
     // string that has the .bin filepath (inside the "binaries" directory)
     char *vehicles_filepath = get_filepath(vehiclesFilename, 'b');
@@ -483,8 +479,6 @@ void append_vehicle_bin_btree(char *vehiclesFilename, char *btreeFilename, int n
     return;
 }
 
-// receives a filename, reads and parses all of the
-// binary file's registers and prints the parsed data
 void print_vehicle_bin(char *filename){
     // string that has the .bin filepath (inside the "binaries" directory)
     char *filepath = get_filepath(filename, 'b');
@@ -575,9 +569,6 @@ void print_vehicle_bin(char *filename){
     free(filepath);
 }
 
-// receives a filename as well as a query (field key-value pair),
-// parses all of the binary file's registers and prints every
-// register that matches the query's parsed data
 void search_vehicle_bin(char *filename, char *key, char *value){
     // string that has the .bin filepath (inside the "binaries" directory)
     char *filepath = get_filepath(filename, 'b');
@@ -687,8 +678,6 @@ void search_vehicle_bin(char *filename, char *key, char *value){
     free(filepath);
 }
 
-// sorts and removes logically deleted register from
-// the original binary then writes it to a new files
 void sort_vehicles_bin(char *originalFilename, char *sortedFilename){
     FILE *f_original = open_validate_binary(originalFilename, "rb");
 
