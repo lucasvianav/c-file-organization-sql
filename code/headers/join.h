ea77be1e5d4e3470e1b7afae286156e88c80d228
Lucas Viana <oviana@outlook.com>
2021-07-23 16:50:13 -0300

    adds comments to join.h

--- code/headers/join.h
+++ code/headers/join.h
@@ -8,10 +8,16 @@
 #ifndef JOIN_H
 #define JOIN_H
 
+// joins a vehicle binary file's data with a line binary file's data
+// uses two nested loops to perform the join operations
 void nested_loop_join(char *vehiclesFilename, char *linesFilename);
 
+// joins a vehicle binary file's data with a line binary file's data
+// uses a single loop with a btree index for the lines file
 void single_loop_join(char *vehiclesFilename, char *linesFilename, char* linesBtreeFilename);
 
+// joins a vehicle binary file's data with a line binary file's data
+// uses a single loop with sort-interpolate strategy
 void sorted_interpolation_join(char *vehiclesFilename, char *linesFilename);
 
 #endif
